eab4aacab297e372d131c6a25be4d3f89613eb56
szapp <dev.szapp@gmail.com>
2019-03-03 15:03:50 +0100

    Initialize patch

---
+++ Ninja/KeepTraderWeapons/Content/NinjaInit.d
@@ -0,0 +1,6 @@
+/*
+ * Initialization function called by Ninja after "Init_Global" (G2) / "Init_<Levelname>" (G1)
+ */
+func void Ninja_KeepTraderWeapons_Init() {
+    FixEquipBestWeapons_Init();
+};
